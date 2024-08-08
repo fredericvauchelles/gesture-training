9eb1bef7ecb54d22aa7a105018433704a02e4582
Frédéric Vauchelles <frederic@vauchelles.com>
2024-08-08 23:11:30 +0200

    WIP using slint

--- gesture-training/build.rs
+++ gesture-training/build.rs
@@ -1,3 +1,6 @@
 fn main() {
-    slint_build::compile("ui/appwindow.slint").unwrap();
+    let config = slint_build::CompilerConfiguration::new().with_style("cosmic-dark".into());
+
+    slint_build::compile_with_config("ui/pages/appwindow.slint", config).unwrap();
+    //slint_build::compile("ui/pages/session.slint").unwrap();
 }
